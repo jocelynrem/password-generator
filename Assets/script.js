b5d43468d884c4382fb5d0a2cd1f18f8cc74f811
jocelynrem <jocelynrem@gmail.com>
2021-04-12 20:57:31 -0400

    fixed error in line 9

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,10 @@ let generateBtn = document.querySelector("#generate");
 
 
 // Declares Possible password characters
-let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let alphaUpcase = alpha.join('$$').toUpperCase().split('$$');
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '/', '?' '[', ']', ' ', '{', '}', '|', '`', '~', '=', '+', ';', ':']
+let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '/', '?', ']', ' ', '{', '}', '|', '`', '~', '=', '+', ';', ':'];
 
 //Function that genenerates the password
 function generatePassword() {
